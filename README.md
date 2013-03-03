8119e175441f1dc3919ae1ecdf07163049e5f4ad
Mike <vantil.mike@gmail.com>
2013-03-03 16:44:05 -0600

    Initial check-in.

---
+++ README.md
@@ -0,0 +1,8 @@
+#luminous-file-resource-type-loader
+=======================
+
+## Documentation
+
+Refer to the root luminous projects at <a href="luminous">npmjs.org/packages/luminous</a> for detailed documentation.
+
+This component loads file based resources for generating html templates.
---
+++ specs/test.spec.js
@@ -0,0 +1,19 @@
+var TypeLoader = require('../lib/luminous-file-resource-type-loader'),
+    fs = require('fs');
+
+describe("Luminous File typeLoader suite", function() {
+    var typeLoader = new TypeLoader();
+
+    it("must be able to load templates", function(done) {
+        var fileReadResult = 'Fake result';
+        spyOn(fs, 'readFile').andCallFake(function(fileName, callback) {
+            expect(fileName).toBe('typeLoader/string.ko');
+            callback(null, fileReadResult);
+        });
+        typeLoader.load('/string', function(err, typeLoader) {
+            expect(err).toBeFalsy();
+            expect(typeLoader).toBe(fileReadResult);
+            done();
+        });
+    }, 1000);
+});
