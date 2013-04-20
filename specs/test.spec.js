c745d8f20ee1b1659e8b50c8ef8eed209053a3e0
Mike <vantil.mike@gmail.com>
2013-04-19 22:53:08 -0500

    Update to recurse into type hierarchy.

--- specs/test.spec.js
+++ specs/test.spec.js
@@ -5,7 +5,7 @@ describe("Luminous File typeLoader suite", function() {
     var typeLoader = new TypeLoader();
 
     it("must be able to load templates", function(done) {
-        var fileReadResult = 'Fake result';
+        var fileReadResult = 'string result';
         spyOn(fs, 'readFile').andCallFake(function(fileName, callback) {
             expect(fileName).toBe('template/string.ko');
             callback(null, fileReadResult);
@@ -16,4 +16,22 @@ describe("Luminous File typeLoader suite", function() {
             done();
         });
     }, 1000);
+
+    it("must be able to load parent templates", function(done) {
+        var fileReadResult = 'Enum result';
+        spyOn(fs, 'readFile').andCallFake(function(fileName, callback) {
+            if (fileName == 'template/enum/test.ko') {
+                return callback(new Error('ENOENT'));
+            }
+            else if (fileName == 'template/enum.ko') {
+                return callback(null, fileReadResult);
+            }
+            callback(new Error('Unexpected file request: ' + fileName));
+        });
+        typeLoader.load('/enum/test', function(err, typeLoader) {
+            expect(err).toBeFalsy();
+            expect(typeLoader).toBe(fileReadResult);
+            done();
+        })
+    });
 });
